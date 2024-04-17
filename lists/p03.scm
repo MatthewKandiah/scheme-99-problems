e29020f9188d562490aa77f69c4d8ba13d4f6e01
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-04-17 20:03:24 +0100

    problem 3

---
+++ lists/p03.scm
@@ -0,0 +1,11 @@
+#|
+Find the K'th element of a list
+|#
+
+(define (element-at lst k)
+  (define (aux lst count)
+	(if (= 1 count)
+	  (car lst)
+	  (aux (cdr lst) (- count 1))))
+  (aux lst k)
+  )
