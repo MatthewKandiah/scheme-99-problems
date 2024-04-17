9fae9b35fa67b4cc97efc28fcda808b79aade127
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-04-17 20:19:12 +0100

    problem 4

---
+++ lists/p04.scm
@@ -0,0 +1,10 @@
+#|
+Find the number of elements of a list.
+|#
+
+(define (length lst)
+  (define (aux l count)
+	(cond ((null? l) 0)
+		  ((null? (cdr l)) (+ count 1))
+		  (else (aux (cdr l) (+ count 1)))))
+  (aux lst 0))
