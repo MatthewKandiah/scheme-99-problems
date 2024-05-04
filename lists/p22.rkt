50a4b3fbf93c6c7c48747ee79a35145ff6eea824
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-05-04 19:39:08 +0100

    p22

---
+++ lists/p22.rkt
@@ -0,0 +1,14 @@
+#lang racket
+
+(define (range low high)
+  (define (aux out cnt)
+	(if (> cnt high)
+	  out
+	  (aux (cons cnt out) (+ 1 cnt))))
+  (cond
+	[(> low high) (display "RANGE ERROR: low > high")]
+	[else (reverse (aux '() low))]))
+
+(require rackunit)
+
+(check-equal? (range 4 9) '(4 5 6 7 8 9))
